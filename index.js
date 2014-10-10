2e3ad418e8e387b7c46fa0e2a958b48885f6131b
Jimmy Hillis <jimmy@hillis.me>
2014-10-10 11:09:54 +0800

    Add support for ignoring media queries and font-size rules for responsive sites

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var through2 = require('through2');
 module.exports = function (options) {
   options = _.defaults(options || {}, {
     size: 1,
-    units: 'px pt'
+    units: 'px pt',
+    ignoreMediaQueries: true,
+    ignoreFontSize: true
   });
   return through2.obj(function (file, enc, next) {
     if (file.isNull()) {
@@ -12,14 +14,17 @@ module.exports = function (options) {
       return next();
     }
     var units = options.units.split(' ').join('|');
-    var exp = ['(\\@media \\((max|min)-width: +?)?(\\d+)(', units, ')'].join('');
+    var exp = ['(\\@media \\((?:max|min)-width: +?)?(font-size: ?)?(\\d+)(', units, ')'].join('');
     var re = new RegExp(exp, 'gi');
     var contents = String(file.contents);
     file.contents = new Buffer(contents.replace(re, function (match, $1, $2, $3, $4) {
-      if ($1) {
+      if ((options.ignoreMediaQueries && $1) || (options.ignoreFontSize && $2)) {
         return match;
       }
-      return String((parseInt($3, 10) * options.size)) + $4;
+      if ($2) {
+        return ['font-size:', String((parseInt($3, 10) * options.size)), $4].join('');
+      }
+      return [String((parseInt($3, 10) * options.size)), $4].join('');
     }));
     next(null, file);
   });
